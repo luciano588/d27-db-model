4965d23068eca2ac772c400c0517f0287df0f88e
luciano588 <luciano588.alm@gmail.com>
2021-01-29 00:24:10 +0000

    01-28

--- src/models.py
+++ src/models.py
@@ -8,23 +8,20 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    post_text = Column(String(250), nullable=False)
+    user_id = Column (Integer, ForeignKey('user.id'))
+    media = Relationship ("Media", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Comment(Base):
+    __tablename__ = 'comment    '
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment = Column(String(250), nullable=False)
+    author = relationship("user", back_populates="post", lazy=True)
+    post_id = Column (Integer, ForeignKey('post.id'))
 
     def to_dict(self):
         return {}
